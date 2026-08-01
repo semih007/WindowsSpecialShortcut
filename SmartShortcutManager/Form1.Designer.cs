6234df823fbbd141be2db2ab89fdd8504d502b7b
semih007 <134814989+semih007@users.noreply.github.com>
2026-08-01 09:15:59 +0000

    değişiklik

--- SmartShortcutManager/Form1.Designer.cs
+++ SmartShortcutManager/Form1.Designer.cs
@@ -57,87 +57,87 @@ partial class Form1
         SuspendLayout();
 
         label1.AutoSize = true;
-        label1.Location = new System.Drawing.Point(50, 15);
+        label1.Location = new System.Drawing.Point(20, 20);
         label1.Name = "label1";
         label1.Size = new System.Drawing.Size(78, 15);
         label1.Text = "Bağlantı Adı";
 
-        txtMappingName.Location = new System.Drawing.Point(150, 12);
-        txtMappingName.Size = new System.Drawing.Size(320, 23);
+        txtMappingName.Location = new System.Drawing.Point(150, 17);
+        txtMappingName.Size = new System.Drawing.Size(620, 28);
 
         label2.AutoSize = true;
-        label2.Location = new System.Drawing.Point(50, 46);
+        label2.Location = new System.Drawing.Point(20, 60);
         label2.Name = "label2";
-        label2.Size = new System.Drawing.Size(120, 30);
+        label2.Size = new System.Drawing.Size(110, 30);
         label2.Text = "Servis Adları\r\n(virgülle ayrılmış)";
 
-        txtServiceName.Location = new System.Drawing.Point(150, 43);
-        txtServiceName.Size = new System.Drawing.Size(320, 23);
+        txtServiceName.Location = new System.Drawing.Point(150, 57);
+        txtServiceName.Size = new System.Drawing.Size(620, 28);
 
         label3.AutoSize = true;
-        label3.Location = new System.Drawing.Point(50, 77);
+        label3.Location = new System.Drawing.Point(20, 108);
         label3.Name = "label3";
-        label3.Size = new System.Drawing.Size(90, 30);
+        label3.Size = new System.Drawing.Size(110, 30);
         label3.Text = "EXE Konumları\r\n(virgülle ayrılmış)";
 
-        txtExePath.Location = new System.Drawing.Point(150, 74);
-        txtExePath.Size = new System.Drawing.Size(320, 23);
+        txtExePath.Location = new System.Drawing.Point(150, 105);
+        txtExePath.Size = new System.Drawing.Size(620, 28);
 
         lblExample.AutoSize = true;
-        lblExample.Location = new System.Drawing.Point(150, 105);
+        lblExample.Location = new System.Drawing.Point(150, 145);
         lblExample.Name = "lblExample";
-        lblExample.Size = new System.Drawing.Size(430, 30);
+        lblExample.Size = new System.Drawing.Size(620, 36);
         lblExample.ForeColor = System.Drawing.Color.DimGray;
-        lblExample.Text = "Örnek: wuauserv, bits\r\nÖrnek: C:\\Windows\\System32\\notepad.exe, C:\\Program Files\\App\\app.exe";
+        lblExample.Text = "Örnek servis: wuauserv, bits\r\nÖrnek EXE: C:\\Windows\\System32\\notepad.exe, C:\\Program Files\\App\\app.exe";
 
-        btnAdd.Location = new System.Drawing.Point(480, 12);
-        btnAdd.Size = new System.Drawing.Size(120, 25);
+        btnAdd.Location = new System.Drawing.Point(790, 17);
+        btnAdd.Size = new System.Drawing.Size(120, 30);
         btnAdd.Text = "Eşleştirme Ekle";
         btnAdd.Click += btnAdd_Click;
 
-        btnDelete.Location = new System.Drawing.Point(610, 12);
-        btnDelete.Size = new System.Drawing.Size(120, 25);
+        btnDelete.Location = new System.Drawing.Point(790, 57);
+        btnDelete.Size = new System.Drawing.Size(120, 30);
         btnDelete.Text = "Eşleştirme Sil";
         btnDelete.Click += btnDelete_Click;
 
-        btnStart.Location = new System.Drawing.Point(450, 43);
-        btnStart.Size = new System.Drawing.Size(120, 25);
+        btnStart.Location = new System.Drawing.Point(790, 102);
+        btnStart.Size = new System.Drawing.Size(120, 30);
         btnStart.Text = "Başlat";
         btnStart.Click += btnStart_Click;
 
-        btnStop.Location = new System.Drawing.Point(450, 74);
-        btnStop.Size = new System.Drawing.Size(120, 25);
+        btnStop.Location = new System.Drawing.Point(790, 142);
+        btnStop.Size = new System.Drawing.Size(120, 30);
         btnStop.Text = "Durdur";
         btnStop.Click += btnStop_Click;
 
-        btnCreateShortcut.Location = new System.Drawing.Point(450, 105);
-        btnCreateShortcut.Size = new System.Drawing.Size(120, 25);
+        btnCreateShortcut.Location = new System.Drawing.Point(790, 182);
+        btnCreateShortcut.Size = new System.Drawing.Size(120, 30);
         btnCreateShortcut.Text = "Kısayol Oluştur";
         btnCreateShortcut.Click += btnCreateShortcut_Click;
 
-        listViewPairs.Location = new System.Drawing.Point(12, 140);
-        listViewPairs.Size = new System.Drawing.Size(760, 260);
+        listViewPairs.Location = new System.Drawing.Point(20, 215);
+        listViewPairs.Size = new System.Drawing.Size(890, 250);
         listViewPairs.View = View.Details;
         listViewPairs.FullRowSelect = true;
         listViewPairs.MultiSelect = false;
         listViewPairs.Columns.AddRange(new[] { columnName, columnService, columnExe });
 
         columnName.Text = "Bağlantı";
-        columnName.Width = 150;
+        columnName.Width = 180;
 
         columnService.Text = "Servisler";
-        columnService.Width = 180;
+        columnService.Width = 260;
 
         columnExe.Text = "Uygulamalar (EXE'ler)";
-        columnExe.Width = 410;
+        columnExe.Width = 450;
 
         lblStatus.AutoSize = true;
-        lblStatus.Location = new System.Drawing.Point(12, 410);
+        lblStatus.Location = new System.Drawing.Point(20, 475);
         lblStatus.Size = new System.Drawing.Size(100, 15);
         lblStatus.Text = "Hazır";
 
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(784, 441);
+        ClientSize = new System.Drawing.Size(940, 510);
         Text = "Akıllı Kısayol Yöneticisi V8";
 
         Controls.Add(label1);
